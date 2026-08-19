37228d5fbc870a265669626deadf1abd42f4970d
Your Name <your.email@example.com>
2026-08-19 15:52:02 +0530

    Add UltraMath.java

--- UltraMath.java
+++ UltraMath.java
@@ -1,16 +1,19 @@
 import java.util.*;
 
 public class UltraMath {
-    public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
-        String n1= sc.nextLine();
-        String n2= sc.nextLine();
-        int num1 = Integer.parseInt(n1, 2);
-        int num2 = Integer.parseInt(n2, 2);
-        int result = num1 ^ num2;
-        String binaryResult = Integer.toBinaryString(result);
-        System.out.println(binaryResult);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) == b.charAt(i))
+                System.out.print("0");
+            else
+                System.out.print("1");
+        }
+
         sc.close();
     }
-    
 }
