ba8cb7673a0f38674f471a5cb01f3c9eba792d93
Your Name <your.email@example.com>
2026-08-21 11:21:44 +0530

    Add Hulk.java

--- Hulk.java
+++ Hulk.java
@@ -20,6 +20,3 @@ public class Hulk {
     }
     
 }
-// if n = 1, then his feeling is "I hate it" or 
-// if n = 2 it's "I hate that I love it", and 
-// if n = 3 it's "I hate that I love that I hate it" and so on.
